35631a755ac38217b02ec2debbe223cd4c996abd
Howard Butler <howard@hobu.co>
2019-04-02 13:16:10 -0500

    need cython and packaging

--- scripts/appveyor/build.cmd
+++ scripts/appveyor/build.cmd
@@ -1,5 +1,5 @@
 call "%CONDA_ROOT%\Scripts\activate.bat" base
-call conda install -c conda-forge -y pdal
+call conda install -c conda-forge -y pdal cython packaging
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
