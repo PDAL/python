ae11bb128dfd473a2b7f8094d06c0a20a3bb0105
Pramukta Kumar <pramukta.kumar@gmail.com>
2019-11-18 13:54:46 -0600

    update and expand testcase

--- test/test_pio.py
+++ test/test_pio.py
@@ -36,12 +36,21 @@ dummy_pipeline = """{
 
 class TestPIOBasics(unittest.TestCase):
     def test_pipeline_construction(self):
-        pipeline = (pio.readers.ply(filename="dummyfile.ply") +
+        pipeline = (pio.readers.ply(filename="dummyinput.ply") +
                     pio.filters.outlier(method="statistical", mean_k=16, multiplier=1.0) +
                     pio.filters.range(limits="Classification![7:7]") +
-                    pio.filters.normal() + pio.writers.ply(storage_mode="ascii", precision=4, filename=output,
+                    pio.filters.normal() + pio.writers.ply(storage_mode="ascii", precision=4, filename="dummyoutput.ply",
                                                            dims="X,Y,Z,Red,Green,Blue,NormalX,NormalY,NormalZ"))
 
-        self.assertTrue(isinstance(pipeline, pio.PipelineSpec))
+        self.assertIsInstance(pipeline, pio.PipelineSpec)
         self.assertEqual(len(list(pipeline.stages)), 5)
-        self.assertEqual(json.dumps(pipeline.spec, indent=2, dummy_pipeline)
+        self.assertEqual(json.dumps(pipeline.spec, indent=2), dummy_pipeline)
+
+        pipeline = pipeline + pio.readers.auto(filename="anotherdummmyfile.laz")
+
+        self.assertEqual(len(pipeline.readers), 2) # we can have multiple readers
+
+        null_writer = pio.writers.null()
+        self.assertIsNot(pipeline.writer, null_writer)
+        pipeline = pipeline + null_writer
+        self.assertIs(pipeline.writer, null_writer)
