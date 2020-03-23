5e913c6664551c5fa7d6f615d37d3160fbd1c064
Howard Butler <howard@hobu.co>
2020-03-23 12:57:28 -0500

    macro wip

---
+++ pdal/macros.cmake
@@ -0,0 +1,103 @@
+function(pdal_target_compile_settings target)
+    set_property(TARGET ${target} PROPERTY CXX_STANDARD 11)
+    set_property(TARGET ${target} PROPERTY CXX_STANDARD_REQUIRED TRUE)
+    target_compile_definitions(${target} PRIVATE
+        -DWIN32_LEAN_AND_MEAN)
+    if (MSVC)
+        # check for MSVC 8+
+        if (NOT (MSVC_VERSION VERSION_LESS 1400))
+            target_compile_definitions(${target} PRIVATE
+                -D_CRT_SECURE_NO_DEPRECATE
+                -D_CRT_SECURE_NO_WARNINGS
+                -D_CRT_NONSTDC_NO_WARNING
+                -D_SCL_SECURE_NO_WARNINGS
+            )
+            target_compile_options(${target} PRIVATE
+                # Yes, we don't understand GCC pragmas
+                /wd4068
+                # Nitro makes use of Exception Specifications, which results in
+                # numerous warnings when compiling in MSVC. We will ignore
+                # them for now.
+                /wd4290
+                /wd4800
+                # Windows warns about integer narrowing like crazy and it's
+                # annoying.  In most cases the programmer knows what they're
+                # doing.  A good static analysis tool would be better than
+                # turning this warning off.
+                /wd4267
+                # Annoying warning about function hiding with virtual
+                # inheritance.
+                /wd4250
+                # some templates don't return
+#                /wd4716
+                # unwind semantics
+#                /wd4530
+                # Standard C++-type exception handling.
+                /EHsc
+                )
+        endif()
+
+    endif()
+endfunction()
+
+
+###############################################################################
+# Add a plugin target.
+# _name The plugin name.
+# ARGN :
+#    FILES the source files for the plugin
+#    LINK_WITH link plugin with libraries
+#    INCLUDES header directories
+#
+# The "generate_dimension_hpp" ensures that Dimension.hpp is built before
+#  attempting to build anything else in the "library".
+#
+# NOTE: _name is the name of a variable that will hold the plugin name
+#    when the macro completes
+macro(PDAL_PYTHON_ADD_PLUGIN _name _type _shortname)
+    set(options)
+    set(oneValueArgs)
+    set(multiValueArgs FILES LINK_WITH INCLUDES SYSTEM_INCLUDES)
+    cmake_parse_arguments(PDAL_PYTHON_ADD_PLUGIN "${options}" "${oneValueArgs}"
+        "${multiValueArgs}" ${ARGN})
+    if(WIN32)
+        set(WINSOCK_LIBRARY ws2_32)
+        set(${_name} "libpdal_plugin_${_type}_${_shortname}")
+    else()
+        set(${_name} "pdal_plugin_${_type}_${_shortname}")
+    endif()
+
+    if (WIN32)
+	    list(APPEND ${PDAL_PYTHON_ADD_PLUGIN_FILES} ${PDAL_TARGET_OBJECTS})
+    endif()
+
+    add_library(${${_name}} SHARED ${PDAL_PYTHON_ADD_PLUGIN_FILES})
+    pdal_target_compile_settings(${${_name}})
+    target_include_directories(${${_name}} PRIVATE
+        ${PROJECT_BINARY_DIR}/include
+        ${PDAL_INCLUDE_DIR}
+        ${PDAL_PYTHON_ADD_PLUGIN_INCLUDES}
+    )
+    target_compile_definitions(${${_name}} PRIVATE PDAL_DLL_EXPORT)
+    if (PDAL_PYTHON_ADD_PLUGIN_SYSTEM_INCLUDES)
+        target_include_directories(${${_name}} SYSTEM PRIVATE
+            ${PDAL_PYTHON_ADD_PLUGIN_SYSTEM_INCLUDES})
+    endif()
+    target_link_libraries(${${_name}}
+        PRIVATE
+            ${PDAL_BASE_LIB_NAME}
+            ${PDAL_UTIL_LIB_NAME}
+            ${PDAL_PYTHON_ADD_PLUGIN_LINK_WITH}
+            ${WINSOCK_LIBRARY}
+    )
+
+
+
+    install(TARGETS ${${_name}}
+        LIBRARY DESTINATION ${CMAKE_INSTALL_DIR}
+        )
+    if (APPLE)
+        set_target_properties(${${_name}} PROPERTIES
+            INSTALL_NAME_DIR "@rpath")
+    endif()
+endmacro(PDAL_PYTHON_ADD_PLUGIN)
