bedfc1a30005fea87acffad7314ed954294b59f4
Howard Butler <howard@hobu.co>
2020-11-24 13:06:08 -0600

    CMAKE_INSTALL_DIR

--- pdal/macros.cmake
+++ pdal/macros.cmake
@@ -91,7 +91,7 @@ macro(PDAL_PYTHON_ADD_PLUGIN _name _type _shortname)
             ${WINSOCK_LIBRARY}
     )
     install(TARGETS ${${_name}}
-        LIBRARY DESTINATION ${PROJECT_NAME}
+        LIBRARY DESTINATION ${CMAKE_INSTALL_DIR}
         )
     if (APPLE)
         set_target_properties(${${_name}} PROPERTIES
