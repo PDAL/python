f369e7502376b539951931c9c28fe02526a7b8b8
Howard Butler <hobu.inc@gmail.com>
2024-04-29 16:31:01 -0500

    bump 3.4.1 for release churn

--- src/pdal/__init__.py
+++ src/pdal/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["Pipeline", "Stage", "Reader", "Filter", "Writer", "dimensions", "info"]
-__version__ = '3.4.0'
+__version__ = '3.4.1'
 
 from . import libpdalpython
 from .drivers import inject_pdal_drivers
