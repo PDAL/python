7afdf3ca045a0df26f81eae4e189fef546bbafd4
Howard Butler <hobu.inc@gmail.com>
2024-05-14 20:38:37 -0500

    bump version number to 3.4.2

--- src/pdal/__init__.py
+++ src/pdal/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["Pipeline", "Stage", "Reader", "Filter", "Writer", "dimensions", "info"]
-__version__ = '3.4.1'
+__version__ = '3.4.2'
 
 from . import libpdalpython
 from .drivers import inject_pdal_drivers
