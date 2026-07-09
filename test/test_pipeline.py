cec784a8b9eafaf5eca55576f1ce13ef59b6b024
Howard Butler <hobu.inc@gmail.com>
2026-07-09 08:19:38 -0500

    check headers for FileSpec

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -295,6 +295,7 @@ class TestPipeline:
         spec = {'path':'junk.las', 'headers':{'header1':'header_1', 'header2':'header_2'}, 'query':{'query1':'query_1', 'query2':'query_2'}}
         assert pdal.Reader(spec).type == "readers.las"
         assert pdal.Reader(spec).options['filename']['path'] == "junk.las"
+        assert pdal.Reader(spec).options['filename']['headers']['header2'] == "header_2"
         assert pdal.Writer("foo.las").type == "writers.las"
 
     def test_streamable(self):
